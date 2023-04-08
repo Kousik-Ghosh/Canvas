8d6747e2e520dbe69f419bfa4a09b2c89d616dcc
Kousik-Ghosh <reach.kghosh@gmail.com>
2023-04-09 00:13:52 +0530

    backup

--- Java/Core/FileIO/src/BufferedWriterCharacter.java
+++ Java/Core/FileIO/src/BufferedWriterCharacter.java
@@ -6,17 +6,17 @@ public class BufferedWriterCharacter {
 
     public static void main(String[] args) throws Exception {
         String filePath = "/Users/kousik/Desktop/Mind Canvas/Java/Core/FileIO/src/output.txt";
-        BufferedWriter bfw = new BufferedWriter(new FileWriter(new File(filePath)));
+        BufferedWriter bfw = new BufferedWriter(new FileWriter(new File(filePath), true));
 
-        bfw.write(97);
-        bfw.newLine();
-        bfw.write("kousik ghosh");
-        bfw.newLine();
-        char[] arr = new char[]{'s','o','s'};
-        bfw.write(arr);
-        bfw.flush();
+        // bfw.write(97);
+        // bfw.newLine();
+        // bfw.write("kousik ghosh");
+        // bfw.newLine();
+        // char[] arr = new char[]{'s','o','s'};
+        // bfw.write(arr);
+        // bfw.flush();
 
-        /* append does not work as expected */
+        /* append works if FileWriter(file, append:true) */
         bfw.newLine();
         bfw.append('e');
         bfw.flush();
