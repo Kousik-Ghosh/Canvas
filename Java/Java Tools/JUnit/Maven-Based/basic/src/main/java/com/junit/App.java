8689c05425025f684587c564a8b5fef062aa6d43
Kousik-Ghosh <reach.kghosh@gmail.com>
2023-04-19 16:46:58 +0530

    backup

--- Java/Java Tools/JUnit/Maven-Based/basic/src/main/java/com/junit/App.java
+++ Java/Java Tools/JUnit/Maven-Based/basic/src/main/java/com/junit/App.java
@@ -2,6 +2,27 @@ package com.junit;
 
 public class App{
     public static void main( String[] args ){
-        
+   
     }
+
+    public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+	public double computeCircleArea(double radius) {
+		return Math.PI * radius * radius;
+	}
+
 }
