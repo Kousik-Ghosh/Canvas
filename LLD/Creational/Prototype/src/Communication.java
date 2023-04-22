2624e0c616696c91cc8debe112b6c87c94e39a61
Kousik-Ghosh <reach.kghosh@gmail.com>
2023-04-22 14:37:18 +0530

    backup

---
+++ LLD/Creational/Prototype/src/Communication.java
@@ -0,0 +1,19 @@
+
+public class Communication {
+
+    String communicationType;
+
+    public Communication(String communicationType) {
+        this.communicationType = communicationType;
+    }
+
+    public String getCommunicationType() {
+        return communicationType;
+    }
+
+    public void setCommunicationType(String communicationType) {
+        this.communicationType = communicationType;
+    }
+    
+
+}
